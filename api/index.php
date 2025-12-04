e5e9495158cc3082774c84bc5312ae1a8b7db31f
AlphaIsYour <alphrenoorz@gmail.com>
2025-12-04 20:48:37 +0700

    update for hosting v5.8

--- api/index.php
+++ api/index.php
@@ -6,23 +6,17 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-try {
-    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-    
-    $response = $kernel->handle(
-        $request = Illuminate\Http\Request::capture()
-    );
-    
-    $response->send();
-    
-    $kernel->terminate($request, $response);
-} catch (\Throwable $e) {
-    // Kalau error, tampilkan sebagai JSON
-    header('Content-Type: application/json');
-    echo json_encode([
-        'error' => $e->getMessage(),
-        'file' => $e->getFile(),
-        'line' => $e->getLine(),
-        'trace' => $e->getTraceAsString()
-    ]);
-}
+// Force load routes
+$app->booted(function () use ($app) {
+    $app->make('router')->getRoutes()->refreshNameLookups();
+});
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
