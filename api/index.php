1834b5530b6ad5d265d461f36250dd15dac45255
AlphaIsYour <alphrenoorz@gmail.com>
2025-12-04 20:53:31 +0700

    update for hosting v5.9

--- api/index.php
+++ api/index.php
@@ -1,15 +1,8 @@
 <?php
 
-define('LARAVEL_START', microtime(true));
-
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// Force load routes
-$app->booted(function () use ($app) {
-    $app->make('router')->getRoutes()->refreshNameLookups();
-});
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
